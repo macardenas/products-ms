ece32ea7a81ca8b0d4d5d8d6644b04b307e35ead
Michel Cardenas <michael.torrealba09@gmail.com>
2024-07-25 12:58:39 -0400

    app

---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,8 @@
+export class Product {
+    
+    public id:number;
+
+    public price:string;
+    
+    public name:string;
+}
---
+++ src/products/products.service.ts
@@ -0,0 +1,77 @@
+import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { RpcException } from '@nestjs/microservices';
+import { PrismaClient } from '@prisma/client';
+import { paginationDto } from 'src/common';
+
+@Injectable()
+export class ProductsService extends PrismaClient implements OnModuleInit {
+
+  private readonly logger = new Logger("ProductService")
+
+  onModuleInit() {
+    this.$connect();
+    this.logger.log("Database connect");
+
+  }
+
+  create(createProductDto: CreateProductDto) {
+    return this.product.create({
+      data: createProductDto
+    })
+  }
+
+  async findAll(productpaginatioDto: paginationDto) {
+
+    const { page, limit } = productpaginatioDto
+
+    const totalpage = await this.product.count({where: { available: true }});
+    const lastpage = Math.ceil( totalpage / limit  );
+
+    return {
+      data: await this.product.findMany({
+        where: { available: true },
+        skip: ( page -1 ) * 10,
+        take: limit
+      }),
+      meta:{
+        totalpage: totalpage,
+        page: page,
+        lastpage: lastpage
+      }
+    };
+    //return `This action returns all products`;
+  }
+
+  async findOne(id: number) {
+      const Product = await this.product.findUnique({
+        where: { id,available: true }
+      })
+
+      if(!Product) throw new RpcException({
+        message:`Producto no encontrado con el id: ${id}`,
+        status: HttpStatus.BAD_REQUEST
+      });
+
+      return Product; 
+  }
+
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    await this.findOne(id);
+
+    return this.product.update({
+      where: { id, available: true },
+      data: updateProductDto
+    })
+  }
+
+  async remove(id: number) {
+    await this.findOne(id);
+
+    return this.product.update({
+      where: { id },
+      data: { available: false}
+    })
+  }
+}
